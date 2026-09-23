28a59bdcdda462747afef9ae0909c03ac645ddba
EnzhooCarp1996 <enzhoocarp1996@gmail.com>
2026-09-23 13:53:43 -0300

    se agrego llamadas por consola

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ async function getAllProducts() {
     }
 }
 
-const productsAll = await getAllProducts();
+async function getProductById(id) {
+    try {
+        const res = await fetch(`https://fakestoreapi.com/products/${id}`, {method: "GET"});
+        if (!res.ok) {
+            throw new Error(`Error HTTP! status: ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error(`Error obteniendo producto con ID: ${id}`, error);
+    }
+}
 
-console.log(productsAll);
+const [, , method, endpoint] = process.argv;
+const normalizedEndpoint = endpoint?.replace(/^\/+/, '');
+
+if (method === 'GET' && normalizedEndpoint === 'products') {
+    const products = await getAllProducts();
+    console.log(products);
+} else if (method === "GET" && normalizedEndpoint?.startsWith('products/')) {
+    const id = normalizedEndpoint.split('/')[1];
+    const product = await getProductById(id);
+    console.log(product);
+} else {
+    console.log('Endpoint no válido. Use /products para obtener todos los productos o /products/:id para obtener un producto específico.');
+}
