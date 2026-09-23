fd5b818e697b77e796504d6e68143cd718b9f7fb
EnzhooCarp1996 <enzhoocarp1996@gmail.com>
2026-09-23 09:23:41 -0300

    se realizo la funcion asincronica de obtener productosde la FakeStoreApi

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Proyecto iniciado correctamente");
+async function getAllProducts() {
+    try {
+        const res = await fetch('https://fakestoreapi.com/products', {method: "GET"});
+        if (!res.ok) {
+            throw new Error(`Error HTTP! status: ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error('Error obteniendo productos:', error);
+    }
+}
+
+const productsAll = await getAllProducts();
+
+console.log(productsAll);
