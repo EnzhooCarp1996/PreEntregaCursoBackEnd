1e5eae614b88fd58e186e0b82f7fb83c728e60a0
EnzhooCarp1996 <enzhoocarp1996@gmail.com>
2026-09-24 15:34:19 -0300

    se agrego consultas para agregar y eliminar, asi tambien se cambio el if por un switch

--- index.js
+++ index.js
@@ -1,39 +1,122 @@
 async function getAllProducts() {
-    try {
-        const res = await fetch('https://fakestoreapi.com/products', {method: "GET"});
-        if (!res.ok) {
-            throw new Error(`Error HTTP! status: ${res.status}`);
-        }
-        const data = await res.json();
-        return data;
-    } catch (error) {
-        console.error('Error obteniendo productos:', error);
+  try {
+    const res = await fetch("https://fakestoreapi.com/products", {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error(`Error HTTP! status: ${res.status}`);
     }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error obteniendo productos:", error);
+  }
 }
 
 async function getProductById(id) {
-    try {
-        const res = await fetch(`https://fakestoreapi.com/products/${id}`, {method: "GET"});
-        if (!res.ok) {
-            throw new Error(`Error HTTP! status: ${res.status}`);
-        }
-        const data = await res.json();
-        return data;
-    } catch (error) {
-        console.error(`Error obteniendo producto con ID: ${id}`, error);
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "GET",
+    });
+    if (!res.ok) {
+      throw new Error(`Error HTTP! status: ${res.status}`);
     }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(`Error obteniendo producto con ID: ${id}`, error);
+  }
+}
+
+async function createProduct(productData) {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+    if (!res.ok) {
+      throw new Error(`Error HTTP! status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(`Error creando el producto:`, error);
+  }
+}
+
+async function deleteProduct(id) {
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${id}`, {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      throw new Error(`Error HTTP! status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(`Error eliminando producto con ID: ${id}`, error);
+  }
 }
 
 const [, , method, endpoint] = process.argv;
-const normalizedEndpoint = endpoint?.replace(/^\/+/, '');
+const endpointEstandar = endpoint?.replace(/^\/+/, "");
 
-if (method === 'GET' && normalizedEndpoint === 'products') {
+if (
+  !(
+    endpointEstandar === "products" || endpointEstandar?.startsWith("products/")
+  )
+) {
+  console.log("Endpoint no válido.");
+  process.exit(1);
+}
+
+switch (method) {
+  case "GET":
+    if (endpointEstandar?.startsWith("products/")) {
+      const id = endpointEstandar.split("/")[1];
+      if (!id || isNaN(Number(id))) {
+        console.log(
+          "Endpoint no válido. Use products/:id para consultar un producto.",
+        );
+        break;
+      }
+
+      const product = await getProductById(id);
+      console.log(product);
+      break;
+    }
+
     const products = await getAllProducts();
     console.log(products);
-} else if (method === "GET" && normalizedEndpoint?.startsWith('products/')) {
-    const id = normalizedEndpoint.split('/')[1];
-    const product = await getProductById(id);
-    console.log(product);
-} else {
-    console.log('Endpoint no válido. Use /products para obtener todos los productos o /products/:id para obtener un producto específico.');
+    break;
+  case "POST":
+    if (endpointEstandar === "products") {
+      const products = await createProduct();
+      console.log(products);
+    } else {
+      console.log(
+        "Endpoint no válido. Use products para traer todos los productos o products/:id para traer un solo producto.",
+      );
+    }
+    break;
+  case "DELETE":
+    if (!endpointEstandar.startsWith("products/")) {
+      console.log(
+        "Endpoint no válido. Use products/:id para eliminar el producto.",
+      );
+      break;
+    }
+
+    const id = endpointEstandar.split("/")[1];
+    if (!id || isNaN(Number(id))) {
+      console.log(
+        "Endpoint no válido. Use products/:id para eliminar el producto.",
+      );
+      break;
+    }
+    const product = await deleteProduct(id);
+    console.log("Producto Eliminado: ", product);
+    break;
 }
